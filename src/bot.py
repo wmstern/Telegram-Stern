98b130c996033560eb20f69446fe5536d845b61f
MrSternn <112782332+MrSternn@users.noreply.github.com>
2022-11-12 16:22:09 -0300

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -6,9 +6,16 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def TestCommand(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Tested by: {update.effective_user.first_name}')
 
+async def HelpCommand(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Commands:\n\n/test")
 
 app = ApplicationBuilder().token(os.environ.get("BOT_TOKEN", "")).build()
 
-app.add_handler(CommandHandler("test", TestCommand))
+def main() -> None:
+    """Run bot."""
+    app.add_handler(CommandHandler("test", TestCommand))
+    app.add_handler(CommandHandler("help", HelpCommand))
 
-app.run_polling()
+    app.run_polling()
+
+main()
