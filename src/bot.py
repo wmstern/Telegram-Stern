938d083082aa43a466010343c68aac6650bd8e3a
MrSternn <112782332+MrSternn@users.noreply.github.com>
2022-11-12 16:22:39 -0300

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -9,10 +9,11 @@ async def TestCommand(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 async def HelpCommand(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Commands:\n\n/test")
 
-app = ApplicationBuilder().token(os.environ.get("BOT_TOKEN", "")).build()
-
 def main() -> None:
     """Run bot."""
+
+    app = ApplicationBuilder().token(os.environ.get("BOT_TOKEN", "")).build()
+
     app.add_handler(CommandHandler("test", TestCommand))
     app.add_handler(CommandHandler("help", HelpCommand))
 
