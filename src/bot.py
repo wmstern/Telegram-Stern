49aebc909a0ef47600ed82547441f81efadcc94a
MrSternn <112782332+MrSternn@users.noreply.github.com>
2022-11-12 15:48:22 -0300

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,13 +1,14 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await update.message.reply_text(f'Tested by: {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
+app = ApplicationBuilder().token(os.environ.get("BOT_TOKEN", "")).build()
 
-app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("test", hello))
 
 app.run_polling()
